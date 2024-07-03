51c17c7ae33f84361edf9baf2c5d5a52134a2cc3
issxy <166388065+issxy@users.noreply.github.com>
2024-07-03 22:58:06 +0800

    Update garbage_collection.h

--- src/garbage_collection.h
+++ src/garbage_collection.h
@@ -1,7 +1,10 @@
-#ifndef garbage_collcation_h
-#define garbage_collcation_h
+#ifndef garbage_collcation_h  // 如果未定义 garbage_collcation_h 宏
+#define garbage_collcation_h  // 定义 garbage_collcation_h 宏
 
+// 声明外部函数 free_question_list，用于释放 DNS 问题列表的内存
 extern void free_question_list(DNSQuestion *question_list);
+
+// 声明外部函数 free_response，用于释放 DNS 响应的内存
 extern void free_response(Response *response);
 
-#endif /* garbage_collcation_h */
+#endif /* garbage_collcation_h */  // 结束条件编译块
