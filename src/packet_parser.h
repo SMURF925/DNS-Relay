78c809801934a755a94fb8d1dd4b77b13db2f835
issxy <166388065+issxy@users.noreply.github.com>
2024-07-03 23:08:27 +0800

    Update packet_parser.h

--- src/packet_parser.h
+++ src/packet_parser.h
@@ -1,8 +1,13 @@
-#ifndef packet_parser_h
-#define packet_parser_h
+#ifndef packet_parser_h  // 如果未定义 packet_parser_h 宏
+#define packet_parser_h  // 定义 packet_parser_h 宏
 
+// 声明外部函数 parse_query，用于解析DNS查询
 extern int parse_query(char packet[], DNSHeader* header, DNSQuestion* question);
+
+// 声明外部函数 parse_response，用于解析DNS响应
 extern int parse_response(char packet[], Response* response);
+
+// 声明外部函数 convert_string，用于转换DNS格式字符串
 extern void convert_string(char src[], char des[]);
 
-#endif /* packet_parser_h */
+#endif /* packet_parser_h */  // 结束条件编译块
